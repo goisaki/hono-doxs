c01066616b0669c5e6df56b7a91bc97180d4dc8a
goisaki <goisaki>
2024-07-23 14:14:30 +0900

    feat: parse .md/.mdx

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,20 @@
 import { showRoutes } from 'hono/dev'
 import { createApp } from 'honox/server'
 
-const app = createApp()
+const app = createApp({
+  ROUTES: import.meta.glob(
+    [
+      // defaults
+      '/app/routes/**/!(_*|*.test|*.spec).(ts|tsx|mdx)',
+      '/app/routes/.well-known/!(_*|*.test|*.spec).(ts|tsx|mdx)',
+      // docs
+      '/docs/**/*.(md|mdx)',
+    ],
+    {
+      eager: true,
+    }
+  ),
+})
 
 showRoutes(app)
 
