d40bfdcf76999808a3fcda0e917dd4b7c9338315
goisaki <goisaki>
2024-07-28 13:17:57 +0900

    add @mdx-js/rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import pages from '@hono/vite-cloudflare-pages'
 import adapter from '@hono/vite-dev-server/cloudflare'
+import mdx from '@mdx-js/rollup'
 import honox from 'honox/vite'
+import remarkGfm from 'remark-gfm'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [honox({ devServer: { adapter } }), pages()],
+	plugins: [
+		honox({ devServer: { adapter } }),
+		mdx({
+			jsxImportSource: 'hono/jsx',
+			remarkPlugins: [remarkGfm],
+		}),
+		pages(),
+	],
 })
