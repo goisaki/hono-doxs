03832139a022d83702fa68de4592a8d50b04b063
goisaki <goisaki>
2024-07-29 09:26:13 +0900

    style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,30 @@ import type { Config } from 'tailwindcss'
 export default {
 	content: ['./app/**/*.tsx'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				heading: [
+					'Cal Sans',
+					'ui-sans-serif',
+					'system-ui',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+				text: [
+					'Inter Variable',
+					'ui-sans-serif',
+					'system-ui',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+			},
+		},
 	},
 	plugins: [],
 } satisfies Config
